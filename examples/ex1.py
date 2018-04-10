d6ee277791fc1e7d4a0e5e9cea981ca87ed818b2
David B. Adrian <dawidh.adrian@googlemail.com>
2018-04-10 16:32:42 +0200

    fix example

--- examples/ex1.py
+++ examples/ex1.py
@@ -9,9 +9,11 @@ def train(model, dataset, model_params, cpu, e):
 def test():
     print("function <test> called with:", locals())
 
-def random(model, dataset, model_params, cpu, e):
-    print("function <random> called with:", locals())
+def list_d(dataset, model_params, cpu, e):
+    print("function <list_d> called with:", locals())
 
+def list_m(model, model_params):
+    print("function <list_m> called with:", locals())
 
 if __name__ == "__main__":
     # create menu: base level (L0), a cmd-type
@@ -41,12 +43,16 @@ if __name__ == "__main__":
                                 func=None)
     # L1 (list): add bunch of cmds
     list_models = list_cmd.add_command("models", "List available models",
-                                       func=random)
-    list_datasets = list_cmd.add_command("datasets", "List available datasets",
-                                         func=random)
-    # L2 (list_datasets): add bunch of cmds
-    list_datasets.add_argument('--model', '-m', type=str, default="ComplEx",
+                                       func=list_m)
+    list_models.add_argument('--model', '-m', type=str, default="ComplEx",
                                help="Name of model.")
+    list_models.add_argument('--model_params', '-mp', type=json.loads,
+                               default="{}",
+                               help="Model parameters as dict (json-formatted)")
+    list_datasets = list_cmd.add_command("datasets", "List available datasets",
+                                         func=list_d)
+
+    # L2 (list_datasets): add bunch of cmds
     list_datasets.add_argument('--dataset', '-d', type=str, default="FB20K",
                                help="Name of dataset.")
     list_datasets.add_argument('--model_params', '-mp', type=json.loads,
